fbb190113094ebdb07362bac5a354ce3aabb2ab4
mert-ozturk <mertozturk.seo.yonetim@gmail.com>
2023-12-16 13:08:24 +0300

    islem

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBgdmUieUQMf8IYSqJF0wIoXcGol7CUq_g",
+  authDomain: "chatappreact-c5316.firebaseapp.com",
+  projectId: "chatappreact-c5316",
+  storageBucket: "chatappreact-c5316.appspot.com",
+  messagingSenderId: "335519700011",
+  appId: "1:335519700011:web:acd75ecedf7f1ebfe0466e"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
